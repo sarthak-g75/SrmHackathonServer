78e8e752d5233defd85f19f4f7e4b9054362f8a9
sarthak783891 <gsarthak783891@gmail.com>
2024-04-06 05:45:22 +0530

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app  = express();
-const port = 5000
+const port = process.env.PORT || 5000;
+const cors = require('cors') 
+app.use(cors())
 const connectToMongo = require('./connectToMongo');
 connectToMongo();
 app.use(express.json()) 
