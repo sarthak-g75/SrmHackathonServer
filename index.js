c88fc728ca338b0366b2fa98669fb9425ad95486
sarthak783891 <gsarthak783891@gmail.com>
2024-04-06 05:50:24 +0530

    fixed

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ app.use(cors())
 const connectToMongo = require('./connectToMongo');
 connectToMongo();
 app.use(express.json()) 
-app.get('/',()=>{
-    return res.status(200).send("hello to backend")
-})
+app.get('/', (req, res) => {
+    return res.status(200).send("hello to backend");
+});
 app.use('/api/auth',require('./routes/auth')) 
 app.listen(port , ()=>{
     console.log('app listening to http://localhost:5000');
